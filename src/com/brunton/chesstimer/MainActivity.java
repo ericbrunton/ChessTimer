12f563d27c9d845f9d221ec42dace7f88ec8b113
Eric Brunton <eric.brunton@yahoo.com>
2014-02-17 02:05:33 -0500

    Added Comments
    Added comments and cleaned up a little.

--- src/com/brunton/chesstimer/MainActivity.java
+++ src/com/brunton/chesstimer/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
+	//declare variables
 	TextView player1Timer;
 	TextView player2Timer;
 	
@@ -31,14 +32,15 @@ public class MainActivity extends Activity {
 	int oldTimer1 = 0;
 	int oldTimer2 = 0;
 	
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		//make android title bar disappear
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
 		
+		//initialize variables
 		player1Timer = (TextView) findViewById(R.id.player1TextView);
 		player2Timer = (TextView) findViewById(R.id.player2TextView);
 		
@@ -48,14 +50,18 @@ public class MainActivity extends Activity {
 		player1Button = (Button) findViewById(R.id.player1Button);
 		player2Button = (Button) findViewById(R.id.player2Button);
 		
+		//set what happens when Black button is clicked
 		player1Button.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
+				
+				//if it is the first time being clicked, start the black timer
 				if(player1Timer.getText().toString() == "Start!")
 				{
 					startTimer1();
 					count1Bool = true;
 				}
+				//if the black timer is currently counting, pause and start white timer
 				else if(count1Bool == true)
 				{
 					pauseTimer1();
@@ -63,14 +69,18 @@ public class MainActivity extends Activity {
 			}
 		});
 		
+		//set what happens when White button is clicked
 		player2Button.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
+				
+				//if it is the first time being clicked, start the white timer
 				if(player2Timer.getText().toString() == "Start!")
 				{
 					startTimer2();
 					count2Bool = true;
 				}
+				//if the white timer is currently counting, pause and start white timer
 				else if(count2Bool == true)
 				{
 					pauseTimer2();
@@ -79,14 +89,17 @@ public class MainActivity extends Activity {
 		});
 	}
 	
+	//starts the Black timer for the first time
 	public void startTimer1()
 	{		
 		count1 = new CountDownTimer(30000, 1000) {
 			
+			//count down second by second, update textview
 			public void onTick(long millisUntilFinished) {
 				player1Timer.setText("" + millisUntilFinished / 1000);
 			}
 			
+			//show winners and losers if timer finishes
 			public void onFinish() {
 				player1Timer.setTextSize(50);
 				player2Timer.setTextSize(50);
@@ -98,14 +111,17 @@ public class MainActivity extends Activity {
 		player2Timer.setText("30");
 	}
 	
+	//starts the White timer for the first time
 	public void startTimer2()
 	{	  
 		count2 = new CountDownTimer(30000, 1000) {
 				
+			//count down second by second, update textview
 			public void onTick(long millisUntilFinished) {
 				player2Timer.setText("" + millisUntilFinished / 1000);
 			}
 		
+			//show winners and losers if timer finishes
 			public void onFinish() {
 				player1Timer.setTextSize(50);
 				player2Timer.setTextSize(50);
@@ -117,6 +133,7 @@ public class MainActivity extends Activity {
 		player1Timer.setText("30");
 	}
 
+	//stop Black timer, start White timer
 	public void pauseTimer1()
 	{
 		count1.cancel();
@@ -127,6 +144,7 @@ public class MainActivity extends Activity {
 		count2Bool = true;
 	}
 	
+	//stop White timer, start Black timer
 	public void pauseTimer2()
 	{
 		count2.cancel();
@@ -137,14 +155,17 @@ public class MainActivity extends Activity {
 		count1Bool = true;
 	}
 	
+	//starting a paused Black timer
 	public void newCountDown1(int newTime)
 	{
 		count1 = new CountDownTimer(newTime, 1000) {
 			
+			//count down second by second
 			public void onTick(long millisUntilFinished) {
 				player1Timer.setText("" + millisUntilFinished / 1000);
 			}
 			
+			//if timer finishes, show winners and losers
 			public void onFinish() {
 				player1Timer.setTextSize(50);
 				player2Timer.setTextSize(50);
@@ -154,14 +175,17 @@ public class MainActivity extends Activity {
 		}.start();
 	}
 	
+	//starting a paused White timer
 	public void newCountDown2(int newTime)
 	{
 		count2 = new CountDownTimer(newTime, 1000) {
 			
+			//count down second by second
 			public void onTick(long millisUntilFinished) {
 				player2Timer.setText("" + millisUntilFinished / 1000);
 			}
 			
+			//if timer finises, show winners and losers
 			public void onFinish() {
 				player1Timer.setTextSize(50);
 				player2Timer.setTextSize(50);
@@ -171,7 +195,6 @@ public class MainActivity extends Activity {
 		}.start();
 	}
 	
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
